e7f04b16ac2bffe6b80468c7505a5925d810e4a8
shtorm901 <59515836+shtorm901@users.noreply.github.com>
2024-02-07 19:58:37 +0300

    Добавлена возможность воспроизводить музыку

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-from PyQt6 import QtWidgets, QtGui
+from PyQt6 import QtWidgets
 import sys
 from ui import Ui_MainWindow
-import random
-from buttons import Execute
+
 
 if __name__ == "__main__":
         app = QtWidgets.QApplication(sys.argv)
-                            
         MainWindow = QtWidgets.QMainWindow()
         ui = Ui_MainWindow()
         ui.setupUi(MainWindow)
         MainWindow.show()
 
-        ui.pushButton.clicked.connect(Execute.button_random)
-        ui.pushButton_4.clicked.connect(Execute.button_on)
-        ui.pushButton_5.clicked.connect(Execute.button_off)
-
-        sys.exit(app.exec())
+sys.exit(app.exec())
