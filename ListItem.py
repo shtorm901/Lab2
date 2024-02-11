e80b85814692df438f8440ed94576917cd34df4f
shtorm901 <59515836+shtorm901@users.noreply.github.com>
2024-02-11 23:22:05 +0300

    Add files via upload

--- ListItem.py
+++ ListItem.py
@@ -1,1 +1,5 @@
-MusicList = ['Hero - Skillet', 'Rare - NEFFEX', 'Gladiator - Jann', 'Fight Back - NEFFEX', 'Feel Invincible - Skillet']
+import glob
+
+Music1 = glob.glob('C:/Users/crabt/Downloads/**/*.mp3')
+Music2 = glob.glob('*.mp3')
+MusicAll = Music1 + Music2
--- main.py
+++ main.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
         ui.setupUi(MainWindow)
         MainWindow.show()
 
-sys.exit(app.exec())
+        app.exec()
